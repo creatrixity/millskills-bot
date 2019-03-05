ea1aaf6f54e98097b906ba51ea84b908b63edfe1
creatrixity <creatrixity@gmail.com>
2019-03-05 12:38:55 +0100

    update process webhook call for requirejs.

--- src/listeners/processWebhookCall.js
+++ src/listeners/processWebhookCall.js
@@ -1,21 +1,24 @@
-var secret = "uchiha-uzumaki-san";
-var repo = "/root/millskills-bot/";
+function processWebhookCall (req, res) {
+  var secret = "uchiha-uzumaki-san";
+  var repo = "/root/millskills-bot/";
+  
+  let http = require('http');
+  let crypto = require('crypto');
+  
+  const exec = require('child_process').exec;
+  
+  
+  req.on('data', function(chunk) {
+      let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
-let http = require('http');
-let crypto = require('crypto');
+      if (req.headers['x-hub-signature'] == sig) {
+          exec('cd ' + repo + ' && git pull');
+          exec('npm install')
+          exec('pm2 restart index')
+      }
+  });
 
-const exec = require('child_process').exec;
+  res.end();
+};
 
-http.createServer(function (req, res) {
-    req.on('data', function(chunk) {
-        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
-
-        if (req.headers['x-hub-signature'] == sig) {
-            exec('cd ' + repo + ' && git pull');
-            exec('npm install')
-            exec('pm2 restart index')
-        }
-    });
-
-    res.end();
-}).listen(8080);
+module.exports = processWebhookCall;
